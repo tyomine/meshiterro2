09f9d1e15a0e5f17418a32c343d46ad1a0187181
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-07-30 03:14:43 +0000

    画像投稿機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "homes/about" => "homes#about", as: "about"
+  resources :post_images, only: [:new, :index, :show, :create]
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
